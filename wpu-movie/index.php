4bf96babac187f82ba3ee52b54f556ce642a3e9c
taufiqpop99 <taufiqpop52@gmail.com>
2024-02-19 21:04:49 +0700

    movie-wpu

--- wpu-movie/index.php
+++ wpu-movie/index.php
@@ -17,7 +17,7 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="#">Searh Movie</a>
+                    <a class="nav-link active" aria-current="page" href="#">Search Movie</a>
                 </div>
             </div>
         </div>
@@ -33,10 +33,26 @@
                 </div>
             </div>
         </div>
-
         <hr>
-
         <div class="row" id="movie-list"></div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Movie Search</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
     </div>
 
     <!-- JavaScript -->
--- wpu-movie/js/script.js
+++ wpu-movie/js/script.js
@@ -1,11 +1,85 @@
-$('#search-button').on('click', function () {
+function searchMovie () {
+    $('#movie-list').html('');
     $.ajax({
         url: 'http://omdbapi.com',
         type: 'get',
         dataType: 'json',
         data: {
-            'apikey' : '132wsa',
+            'apikey' : 'ac80c90a',
             's' : $('#search-input').val()
         },
+        success: function (result) {
+            if (result.Response == "True") {
+                let movies = result.Search;
+
+                $.each(movies, function (i, data) {
+                    $('#movie-list').append(`
+                        <div class="col-md-4">
+                            <div class="card mb-3">
+                                <img src="`+ data.Poster +`" class="card-img-top">
+                                <div class="card-body">
+                                    <h5 class="card-title">`+ data.Title +`</h5>
+                                    <h6 class="card-subtitle mb-2 text-body-secondary">`+ data.Year +`</h6>
+                                    <a href="#" class="btn btn-primary see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="`+ data.imdbID +`">See Details</a>
+                                </div>
+                            </div>
+                        </div>
+                    `);
+                });
+
+                $('#search-input').val('');
+
+            } else {
+                $('#movie-list').html(`
+                    <div class="col">
+                        <h1 class="text-center">`+ result.Error +`</h1>
+                    </div>
+                `);
+            }
+        }
+    });
+}
+
+$('#search-button').on('click', function () {
+    searchMovie();
+});
+
+$('#search-input').on('keyup', function (event) {
+    if (event.which === 13) {
+        searchMovie();
+    }
+});
+
+$('#movie-list').on('click', '.see-detail', function () {
+    $.ajax({
+        url: 'http://omdbapi.com',
+        type: 'get',
+        dataType: 'json',
+        data: {
+            'apikey' : 'ac80c90a',
+            'i' : $(this).data('id')
+        },
+        success: function (movie) {
+            if (movie.Response === "True") {
+                $('.modal-body').html(`
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-4>
+                                <img src="`+ movie.Poster +`" class="img-fluid">
+                            </div>
+                            <div class="col-md-8>
+                                <ul class="list-group">
+                                    <li class="list-group-item"><h3>`+ movie.Title +`</h3></li>
+                                    <li class="list-group-item">Released : `+ movie.Released +`</li>
+                                    <li class="list-group-item">Genre : `+ movie.Genre +`</li>
+                                    <li class="list-group-item">Director : `+ movie.Director +`</li>
+                                    <li class="list-group-item">Actors : `+ movie.Actors +`</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                `);
+            }
+        }
     });
 });
